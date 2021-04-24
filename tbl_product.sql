4ec951f05a087b9f42386c39cb96a5352a2e78a8
Mayuri shirude <83109391+ShirudeMayuri@users.noreply.github.com>
2021-04-24 13:59:00 +0530

    Add files via upload

---
+++ tbl_product.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2021 at 11:50 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chasma_ghar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `category` int(255) NOT NULL,
+  `subcategory` int(255) NOT NULL,
+  `tax_name` varchar(255) NOT NULL,
+  `tax_percentage` varchar(255) NOT NULL,
+  `HSN_code` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `size` varchar(50) NOT NULL,
+  `price` int(255) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `product_delete` int(11) NOT NULL,
+  `status` enum('Y','N') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`id`, `product_name`, `category`, `subcategory`, `tax_name`, `tax_percentage`, `HSN_code`, `description`, `image`, `quantity`, `size`, `price`, `created_date`, `product_delete`, `status`) VALUES
+(2, 'sony', 1, 1, '1', '2', '1', '<p>dfgvdfgvdf</p>', 'login.jpg', 1, 'large', 210000, '2021-04-17 14:21:03', 0, 'N'),
+(3, 'hp', 2, 2, '2', '3', '1', '<p>erter41</p>', 'ba1.jpg', 12, 'large1', 2100001, '2021-04-17 17:14:48', 0, 'Y');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
